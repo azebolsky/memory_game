5073b507fb2e48eddee1d085fd8ae4d387b09201
ADAM <zebolsky.adam@gmail.com>
2020-01-08 11:21:33 -0600

    committed html and js fileschanges

--- js/main.js
+++ js/main.js
@@ -3,8 +3,17 @@ let cardsInPlay = [];
 
 let cardOne = cards[0];
 cardsInPlay.push(cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
 console.log("User flipped queen");
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
