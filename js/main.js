86aedebc559f35ac819308e6e92876a585c1f7a6
ADAM <zebolsky.adam@gmail.com>
2020-01-14 11:30:23 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,45 +2,60 @@ let cards = [
 {
 	rank: "queen",
 	suit: "hearts",
-	cardImage: "image/queen-of-hearts.png"
+	cardImage: "images/queen-of-hearts.png"
 },
 {
 	rank: "queen",
 	suit: "diamonds",
-	cardImage: "image/queen-of-diamonds.png"
+	cardImage: "images/queen-of-diamonds.png"
 },
 {
 	rank: "king",
 	suit: "hearts",
-	cardImage: "image/king-of-hearts.png"
+	cardImage: "images/king-of-hearts.png"
 },
 {
 	rank: "king",
 	suit: "diamonds",
-	cardImage: "image/king-of-diamonds.png"
+	cardImage: "images/king-of-diamonds.png"
 }
 ];
+//Monitor cards in play
 let cardsInPlay = [];
 
-function checkForMatch() {
-	if (cardsInPlay.length === 2) {
-		if (cardsInPlay[0] === cardsInPlay[1]) {
-			alert("You found a match!");
-		}
-		else {
-			alert("Sorry, try again.");
-		}
+//check for card match
+function checkForMatch () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
 	}
 }
 
-function flipCard(cardId) {
+function createBoard () {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+//flip card to check pair
+function flipCard () {
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	cardsInPlay.push(cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
+
 
 
